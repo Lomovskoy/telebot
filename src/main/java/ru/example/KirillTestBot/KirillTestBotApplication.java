c98c58882b24cec34d02749c00d05be603c26858
Lomovskoy <lomovskoy.kirill@yandex.ru>
2025-04-21 19:20:35 +0300

    1. Init

---
+++ src/main/java/ru/example/KirillTestBot/KirillTestBotApplication.java
@@ -0,0 +1,13 @@
+package ru.example.KirillTestBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirillTestBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirillTestBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/example/KirillTestBot/KirillTestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.example.KirillTestBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KirillTestBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
